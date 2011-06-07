8404ff5ae4b0995b9c7c183150234254ae3fa023
John Anderson <sontek@gmail.com>
2011-06-06 19:05:51 -0500

    refactored into a class, added smtp options

--- pymemmon.py
+++ pymemmon.py
@@ -4,104 +4,146 @@ import signal
 import argparse
 import logging
 
-def run():
-    parser = argparse.ArgumentParser(description='Kill runaway processes')
-    parser.add_argument('-w', '--whitelist', help='Comma separated list of processes to monitor')
-    parser.add_argument('-b', '--blacklist', help='Comma separated list of processes to ignore')
-    parser.add_argument('-m', '--max-memory', help='Max memory for a process in megabytes, default is 100mb')
-    parser.add_argument('-s', '--signal', help='The signal to send to the process, default is SIGUSR1')
-    parser.add_argument('-l', '--log-level', help='The amount of logging you want: INFO, DEBUG, or ERROR')
-    parser.add_argument('-f', '--log-file', help='File to log to')
+class Memmon(object):
+    def __init__(self):
+        parser = argparse.ArgumentParser(description='Kill runaway processes')
+        parser.add_argument('-w', '--whitelist', help='Comma separated list of processes to monitor, will only monitor these if defined')
+        parser.add_argument('-b', '--blacklist', help='Comma separated list of processes to ignore')
+        parser.add_argument('-m', '--max-memory', help='Max memory for a process in megabytes, default is 100mb')
+        parser.add_argument('-s', '--signal', help='The signal to send to the process, default is SIGUSR1')
+        parser.add_argument('-l', '--log-level', help='The amount of logging you want: INFO, DEBUG, or ERROR')
+        parser.add_argument('-f', '--log-file', help='File to log to')
+        parser.add_argument('-e', '--send-mail', action='store_true', help='Will attempt to notify via e-mail if this is set')
+        parser.add_argument('--smtp-host', help='The smtp server address you want to send notifications through')
+        parser.add_argument('--smtp-port', help='The port the smtp server listens on')
+        parser.add_argument('--smtp-user', help='The username to authenticate as')
+        parser.add_argument('--smtp-password', help='The password to use for the smtp server')
 
-    args = parser.parse_args(sys.argv[1:])
-    check_processes(args.max_memory, args.whitelist, args.blacklist, args.signal, args.log_level, args.log_file)
+        args = parser.parse_args(sys.argv[1:])
 
-def check_processes(max_memory, whitelist, blacklist, sig, log_level, log_file):
-    # set sane defaults
-    if  max_memory:
-        # convert arg to bytes
-        max_memory = max_memory * (1024 * 1024)
-    else:
-        # default max memory to 100mb
-        MAX_MEM = 100 * (1024 * 1024)
-        max_memory = MAX_MEM
+        # set sane defaults
+        self.whitelist = None
+        self.blacklist = ['mds']
 
-    if sig:
-        sig = getattr(signal, sig.strip().upper(), signal.SIGUSR1)
-    else:
-        sig = signal.SIGUSR1
+        self.should_send_mail = False
+        if args.send_mail:
+            self.should_send_mail = True
 
-    if log_level:
-        log_level = getattr(logging, log_level.strip().upper(), logging.ERROR)
-    else:
-        log_level = logging.ERROR
+        if  args.max_memory:
+            # convert arg to bytes
+            self.max_memory = args.max_memory * (1024 * 1024)
+        else:
+            # default max memory to 100mb
+            MAX_MEM = 100 * (1024 * 1024)
+            self.max_memory = MAX_MEM
 
-    LOG_FORMAT='%(asctime)s %(message)s'
+        if args.signal:
+            self.sig = getattr(signal, args.signal.strip().upper(), signal.SIGUSR1)
+        else:
+            self.sig = signal.SIGUSR1
 
-    if log_file:
-        logging.basicConfig(format=LOG_FORMAT, level=log_level, filename=log_file)
-    else:
-        logging.basicConfig(format=LOG_FORMAT, level=log_level)
+        if args.log_level:
+            self.log_level = getattr(logging, args.log_level.strip().upper(), logging.ERROR)
+        else:
+            self.log_level = logging.ERROR
 
-    if whitelist:
-        whitelist = whitelist.strip().split(',')
+        self.LOG_FORMAT='%(asctime)s %(message)s'
 
-    if blacklist:
-        blacklist = blacklist.strip().split(',')
+        if args.log_file:
+            logging.basicConfig(format=self.LOG_FORMAT, level=args.log_level, filename=args.log_file)
+        else:
+            logging.basicConfig(format=self.LOG_FORMAT, level=args.log_level)
 
-    procs = psutil.get_process_list()
+        if args.whitelist:
+            self.whitelist = args.whitelist.strip().split(',')
 
-    for proc in procs:
-        try:
-            # if a whitelist is defined, make sure the process is in it
-            if whitelist:
-                if not proc.name in whitelist:
-                    continue
+        if args.blacklist:
+            self.blacklist += args.blacklist.strip().split(',')
 
-            # if a blacklist is defined, make sure the process is not in it
-            if blacklist:
-                if proc.name in blacklist:
-                    continue
+        if args.smtp_host:
+            self.smtp_host = args.smtp_host.strip()
+        else:
+            self.smtp_host = 'localhost'
 
-            mem = proc.get_memory_info()[0]
-            if mem >= max_memory:
-                logging.info('Killing %s at memory %s mb' % (proc.name, mem / (1024 * 1024)))
-                name = proc.name
-                # keep sending the signal until the process dies
-                IS_ALIVE = True
-                while IS_ALIVE:
-                    try:
-                        proc.send_signal(sig)
-                    except psutil.error.NoSuchProcess:
-                        logging.info('Process %s has been killed' % name)
-                        IS_ALIVE = False
+        if args.smtp_port:
+            try:
+                self.smtp_port = int(args.smtp_port.strip())
+            except:
+                self.smtp_port = 25
+        else:
+            self.smtp_port = 25
 
-#                send_mail(proc.name, mem)
-        except psutil.error.AccessDenied as exc:
-            logging.error('Do not have access to %s' % exc.msg)
-            pass
+        if args.smtp_user:
+            self.smtp_user = args.smtp_user.strip()
 
-#def send_mail(proc, mem):
-#    import smtplib
-#    from email.MIMEText import MIMEText
-#    msg = MIMEText('Had to kill %(process)s it was using %(memory)s megs of memory' % {
-#        'process': proc,
-#        'memory': (mem / 1024) / 1024,
-#    })
-#    
-#    sender = 'sontek@gmail.com'
-#    recipient = 'sontek@gmail.com'
-#
-#    msg['Subject'] = 'Killed a process' 
-#    msg['From'] = sender
-#    msg['To'] = recipient
-#
-#    s = smtplib.SMTP()
-#    s.connect()
-#    s.sendmail(sender, [recipient], msg.as_string())
-#    s.close()
-#
+        if args.smtp_password:
+            self.smtp_password = args.smtp_password.strip()
+
+    def check_processes(self):
+        procs = psutil.get_process_list()
+
+        for index, proc in enumerate(procs):
+            try:
+                # if a whitelist is defined, make sure the process is in it
+                if self.whitelist:
+                    if not proc.name in self.whitelist:
+                        continue
+
+                # if a blacklist is defined, make sure the process is not in it
+                if self.blacklist:
+                    if proc.name in self.blacklist:
+                        continue
+
+                mem = proc.get_memory_info()[0]
+
+                if mem >= self.max_memory:
+                    logging.info('Killing %s at memory %s mb' % (proc.name, mem / (1024 * 1024)))
+                    name = proc.name
+                    # keep sending the signal until the process dies
+                    IS_ALIVE = True
+                    while IS_ALIVE:
+                        try:
+                            proc.send_signal(self.sig)
+                        except psutil.error.NoSuchProcess:
+                            logging.info('Process %s has been killed' % name)
+                            IS_ALIVE = False
+
+                    if not IS_ALIVE:
+                        if self.should_send_mail:
+                            self.send_mail(name, mem)
+
+            except psutil.error.AccessDenied as exc:
+                logging.error('Do not have access to %s' % exc.msg)
+                pass
+
+    def send_mail(self, name, mem):
+        import smtplib
+        from email.MIMEText import MIMEText
+
+        msg = MIMEText('Had to kill %(process)s it was using %(memory)s megs of memory' % {
+            'process': name,
+            'memory': mem / (1024 * 1024),
+        })
+
+        recipient = 'sontek@gmail.com'
+
+        msg['Subject'] = 'Killed a process' 
+        msg['From'] = self.smtp_user
+        msg['To'] = recipient
+
+        mailServer = smtplib.SMTP(self.smtp_host, self.smtp_port)
+        mailServer.ehlo()
+        mailServer.starttls()
+        mailServer.ehlo()
+
+        if self.smtp_user and self.smtp_password:
+            mailServer.login(self.smtp_user, self.smtp_password)
+
+        mailServer.sendmail(self.smtp_user, recipient, msg.as_string())
+        mailServer.close()
 
 if __name__ == '__main__':
-    run()
+    monitor = Memmon()
+    monitor.check_processes()
+
 
